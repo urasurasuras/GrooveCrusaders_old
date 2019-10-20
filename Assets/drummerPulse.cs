74d2f88866122dde4b915c8d1a5c07a8623d387f
unknown <uo7@njit.edu>
2019-10-20 17:44:26 -0400

    Drummer now shoots beat pulses that collide and all.

---
+++ Assets/drummerPulse.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class drummerPulse : MonoBehaviour
+{
+    public float growSpeed;
+    public Rigidbody2D rb;
+    private CircleCollider2D drummerPulseCollider;
+    private float circleScale;
+
+    /*
+public float x = 0.1f;
+public float y = 0.1f;
+public float z = 0.1f;
+*/
+    void Start()
+    {
+        this.drummerPulseCollider = GetComponent<CircleCollider2D>();
+    }
+    void Update()
+    {
+        // Widen the object by x, y, and z values
+        transform.localScale += new Vector3(growSpeed, growSpeed, growSpeed);
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+        }
+        else
+        {
+            Debug.Log("circle collided with: "+other.gameObject.tag);
+            gameObject.SetActive(false);
+            Destroy(this);
+        }
+    }
+}
---
+++ Assets/weaponDrumset.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weaponDrumset : MonoBehaviour
+{
+    public GameObject drumPulseObject;   //projectile prefab
+    public Transform firePoint;         //position from where to fire 
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space) /*&& playerCanFire*/)
+        {
+            fire();
+            //playerCanFire = false;
+        }
+    }
+
+    void fire()
+    {
+        Instantiate(drumPulseObject, firePoint.position, firePoint.rotation);
+    }
+}
