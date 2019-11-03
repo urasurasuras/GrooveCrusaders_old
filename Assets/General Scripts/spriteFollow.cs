4bade9b3553c709daecb19daee699fd768fc7b4b
unknown <uo7@njit.edu>
2019-11-02 21:41:35 -0400

    Sprite that follows the target object and faces the camera angle. (big brain solution to 2.5 D perspective)

---
+++ Assets/General Scripts/spriteFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spriteFollow : MonoBehaviour
+{
+    //public GameObject objectOfSprite;
+    public Transform positionTarget;    //used to follow gameObject
+    public Transform angleTarget;       //used face the camera
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //posOfObj = objectOfSprite.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = positionTarget.position;
+        transform.rotation = angleTarget.rotation;
+    }
+}
