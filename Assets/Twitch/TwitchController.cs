822dc7b50eeb8b211a50c62a0f753d156d53e2bd
unknown <uo7@njit.edu>
2019-11-05 19:55:26 -0500

    Added Twitch controller.

---
+++ Assets/Twitch/TwitchController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.ComponentModel;
+using System.Net.Sockets;
+using System.IO;
+
+public class TwitchController : MonoBehaviour
+{
+    private TcpClient twitchClient;
+    private StreamReader reader;
+    private StreamWriter writer;
+
+    public string username, password, channelname; //https://twitchapps.com/tmi/
+    // Start is called before the first frame update
+    void Start()
+    {
+        Connect();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!twitchClient.Connected)    //connect if not
+        {
+            Connect();
+        }
+        ReadChat();
+    }
+
+    private void Connect()
+    {
+        twitchClient = new TcpClient("irc.chat.twitch.tv", 6667);
+
+        reader = new StreamReader(twitchClient.GetStream());
+        writer = new StreamWriter(twitchClient.GetStream());
+
+        writer.WriteLine("PASS " + password);
+        writer.WriteLine("NICK " + username);
+        writer.WriteLine("USER " + username + " 8 * :" + username);
+        writer.WriteLine("JOIN #" + channelname);
+        writer.Flush();
+    }
+
+    private void ReadChat()
+    {
+        if(twitchClient.Available > 0)
+        {
+            var message = reader.ReadLine();
+
+            if (message.Contains("PRIVMSG"))
+            {
+                //strip name
+                var splitPoint = message.IndexOf("!", 1);
+                var chatName = message.Substring(0, splitPoint);
+                chatName = chatName.Substring(1);
+
+                //strip message
+                splitPoint = message.IndexOf(":", 1);
+                message = message.Substring(splitPoint + 1);
+                print(String.Format("{0}: {1}", chatName, message));
+            }
+        }
+    }
+}
