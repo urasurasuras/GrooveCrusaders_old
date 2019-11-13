4c33be1d5cf8f392a63fcfd6277ea265b2e73c15
unknown <uo7@njit.edu>
2019-11-13 13:58:43 -0500

    fixed inheritence error

--- Assets/Characters/Player3/weaponBass.cs
+++ Assets/Characters/Player3/weaponBass.cs
@@ -4,26 +4,5 @@ using UnityEngine;
 
 public class weaponBass : weapon
 {
-    public GameObject liteObject;   //projectile prefab
-    public Transform firePoint;     //position from where to fire 
-    public bool bassCanFire;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (bassCanFire)
-        {
-            fire();
-            bassCanFire = false;
-        }
-    }
-    void fire()
-    {
-        Instantiate(liteObject, firePoint.position, firePoint.rotation);
-    }
+    
 }
