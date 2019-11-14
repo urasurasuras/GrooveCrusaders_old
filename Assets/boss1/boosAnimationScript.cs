dfde5cc08556c6cb2327e2fc71bf1d7e43974af3
unknown <uo7@njit.edu>
2019-11-14 13:42:01 -0500

    boss1 animation transforms entire object

--- Assets/boss1/boosAnimationScript.cs
+++ Assets/boss1/boosAnimationScript.cs
@@ -7,9 +7,9 @@ public class boosAnimationScript : MonoBehaviour
 {
     Animator bossAnimationController;
     public BoxCollider2D centerCollider;
-    public BoxCollider2D rightSideCollider;
-    public BoxCollider2D leftSideCollider;
-    public BoxCollider2D finalPhaseCollider;
+    //public BoxCollider2D rightSideCollider;
+    //public BoxCollider2D leftSideCollider;
+    //public BoxCollider2D finalPhaseCollider;
 
     public GameObject enemyAdd;
     public float boss1Health = 100;
@@ -19,25 +19,33 @@ public class boosAnimationScript : MonoBehaviour
 
     public float dmgTaken = 10;
 
+    void Start()
+    {
+        noteShooter.barEvent.AddListener(spawnEnemyAdd);
+        bossAnimationController = GetComponent<Animator>();
+        centerCollider = GetComponent<BoxCollider2D>();
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag =="f_damage")
         {
-            Debug.Log(this + " hit by "+other);
+            //Debug.Log(this + " hit by "+other);
             if(boss1Health < maxHealth)
             {
                 bossAnimationController.SetTrigger("RightFight");
-                activateRight();
+                //activateRight();
             }
             if (boss1Health <= stage2health)
             {
                 bossAnimationController.SetTrigger("FinalFight");
-                activateFinal();
+                //activateFinal();
             }
             if (boss1Health <= 0)
             {
                 bossAnimationController.SetTrigger("BossDefeat");
-                onBossDefeat();
+                centerCollider.enabled = false;
+                //onBossDefeat();
             }
             //Debug.Log("boss Hit!!");
 
@@ -46,48 +54,43 @@ public class boosAnimationScript : MonoBehaviour
             bossAnimationController.SetTrigger("RightFight");
         }
     }
-    public void activateCenter()
-    {
-        centerCollider.enabled = true;
-        rightSideCollider.enabled = false;
-        leftSideCollider.enabled = false;
-        finalPhaseCollider.enabled = false;
-    }
+    //public void activateCenter()
+    //{
+    //    centerCollider.enabled = true;
+    //    rightSideCollider.enabled = false;
+    //    leftSideCollider.enabled = false;
+    //    finalPhaseCollider.enabled = false;
+    //}
     public void activateRight()
     {
-        rightSideCollider.enabled = true;
-        centerCollider.enabled = false;
-        leftSideCollider.enabled = false;
-        finalPhaseCollider.enabled = false;
+    //    rightSideCollider.enabled = true;
+    //    centerCollider.enabled = false;
+    //    leftSideCollider.enabled = false;
+    //    finalPhaseCollider.enabled = false;
     }
-    public void activateLeft()
-    {
-        rightSideCollider.enabled = true;
-        centerCollider.enabled = false;
-        leftSideCollider.enabled = false;
-        finalPhaseCollider.enabled = false;
-    }
+    //public void activateLeft()
+    //{
+    //    rightSideCollider.enabled = true;
+    //    centerCollider.enabled = false;
+    //    leftSideCollider.enabled = false;
+    //    finalPhaseCollider.enabled = false;
+    //}
     public void activateFinal()
     {
-        finalPhaseCollider.enabled = true;
-        rightSideCollider.enabled = false;
-        centerCollider.enabled = false;
-        leftSideCollider.enabled = false;
+    //    finalPhaseCollider.enabled = true;
+    //    rightSideCollider.enabled = false;
+    //    centerCollider.enabled = false;
+    //    leftSideCollider.enabled = false;
     }
-    public void onBossDefeat()
-    {
-        finalPhaseCollider.enabled = false;
-        rightSideCollider.enabled = false;
-        centerCollider.enabled = false;
-        leftSideCollider.enabled = false;
-    }
+    //public void onbossdefeat()
+    //{
+    //    finalphasecollider.enabled = false;
+    //    rightsidecollider.enabled = false;
+    //    centercollider.enabled = false;
+    //    leftsidecollider.enabled = false;
+    //}
     // Start is called before the first frame update
-    void Start()
-    {
-        noteShooter.barEvent.AddListener(spawnEnemyAdd);
-        bossAnimationController = GetComponent<Animator>();
-    }
-
+    
     void spawnEnemyAdd()
     {
         Instantiate(enemyAdd, new Vector3(0, 0, 0),Quaternion.identity);
