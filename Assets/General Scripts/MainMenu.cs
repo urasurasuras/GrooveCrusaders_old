9aac6b76cc27d4fd6a6015469c4543cf38fe2037
unknown <uo7@njit.edu>
2019-12-07 14:31:10 -0500

    Play button, tutorial toggle with main menu GO

---
+++ Assets/General Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public static bool play_tut = true;
+    public void ask_tut(bool toggle)   //Change bool based on toggle checkbox
+    {
+        play_tut = toggle;
+    }
+    public void playGame()
+    {
+        if (play_tut)
+        {
+            SceneManager.LoadScene(1);
+        }
+        else
+            SceneManager.LoadScene(2);
+    }
+}
