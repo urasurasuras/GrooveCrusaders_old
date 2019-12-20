e3e1273655d3328a1b9d0d32feb27eab94346666
Uras Oran <uo7@njit.edu>
2019-12-20 01:20:22 -0500

    Added character-controller gui
    This GUI displays controller names that will correspond to each character

---
+++ Assets/General Scripts/ControllerGui.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControllerGui : MonoBehaviour
+{
+    public Text cont0, cont1, cont2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        try
+        {
+            cont0.text = Input.GetJoystickNames()[0];
+        }catch (System.IndexOutOfRangeException e)
+        {
+            cont0.text = "No controller connected, move by WASD";
+        }
+        try
+        {
+            cont1.text = Input.GetJoystickNames()[1];
+        }catch (System.IndexOutOfRangeException e)
+        {
+            cont1.text = "No controller connected, move by WASD";
+        }
+        try
+        {
+            cont2.text = Input.GetJoystickNames()[2];
+        }catch (System.IndexOutOfRangeException e)
+        {
+            cont2.text = "No controller connected, move by WASD";
+        }        
+    }
+}
